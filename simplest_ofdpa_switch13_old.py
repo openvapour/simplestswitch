8a35d6ff61f983aa6c424e7a3bfb27edb1c4ba62
Richard Sanger <rsanger@wand.net.nz>
2016-06-20 13:52:22 +1200

    Updates the ofdpa simplest switch to work with a newer pipeline
    * Updates ofdpa simplest switch to work with the Atrium 16A ofdpa release

--- simplest_ofdpa_switch13_old.py
+++ simplest_ofdpa_switch13_old.py
@@ -15,17 +15,10 @@
 # limitations under the License.
 
 """
-A very basic OpenFlow 1.3 2 port switch for broadcom's of-dpa 2.X pipeline
-This is working on the Atrium 16A release of of-dpa found here:
-https://github.com/onfsdn/atrium-docs/tree/master/16A/ONOS/builds
-The install guide is here:
-https://wiki.onosproject.org/display/ONOS/Hardware+Switch+Installation+Guide
-
-
-This works by assigning untagged packets to VLAN 1 within the pipeline
-in the VLAN_TABLE this is added. The ACL table is used to select the
-other port as the destination and a l2 interface group to strip the
-tag on egress.
+A very basic OpenFlow 1.3 2 port switch for broadcom's of-dpa pipeline
+This was implemented on the AS4600 of-dpa for the version of ofdpa found here:
+https://github.com/opennetworklinux/ofdpa-2.0-closed-accton/tree/bdb084e89ba2816cd1cd8600ea6a88a275c27afa
+I found this ofdpa to be a bit broken with regards to retriving packet ins.
 """
 
 import logging
@@ -36,14 +29,11 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-import ryu.app.ofctl.api as api
 
 
 class SimplestSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     ACL_TABLE = 60
-    VLAN_TABLE = 10
-    TERMINATION_MAC = 20
 
     def __init__(self, *args, **kwargs):
         super(SimplestSwitch13, self).__init__(*args, **kwargs)
@@ -54,15 +44,10 @@ class SimplestSwitch13(app_manager.RyuApp):
             datapath = ev.msg.datapath
             ofproto = datapath.ofproto
             parser = datapath.ofproto_parser
-            n_vlan = 0x1  # The native vlan to use
-            n_vlan_match = n_vlan | ofproto_v1_3.OFPVID_PRESENT
 
             logging.info("Configuring new datapath 0x%x" % datapath.id)
 
             # clean up all groups and flows
-            # If groups have other groups referencing them this might fail
-            # as they need to be removed in the specific order. (Which is
-            # not the case for groups we install)
             self.del_flows(datapath)
             self.del_groups(datapath)
 
@@ -76,62 +61,39 @@ class SimplestSwitch13(app_manager.RyuApp):
             # output ports
             ports = [1, 2]
             for port in ports:
-                # We first must allow the native vlan and then assign it to
-                # untagged packets
-                match = parser.OFPMatch(in_port=port, vlan_vid=n_vlan_match)
-                actions = []
-                self.add_flow(datapath, 3, match, actions,
-                              table_id=self.VLAN_TABLE,
-                              goto=self.TERMINATION_MAC)
-                # Assign the native VLAN to untagged packets
-                match = parser.OFPMatch(in_port=port, vlan_vid=0)
-                actions = [parser.OFPActionSetField(vlan_vid=n_vlan)]
-                # parser.OFPActionPushVlan(0x8100)
-                # This seems to be implied that it will push a vlan
-                # This is like OF1.0 VLANs
-                self.add_flow(datapath, 3, match, actions,
-                              table_id=self.VLAN_TABLE,
-                              goto=self.TERMINATION_MAC)
-
-            for port in ports:
-                # Make a group of the l2 direct type
-                group_id = self.l2_if_group(port, n_vlan)
-                bkt = [parser.OFPBucket(actions=[parser.OFPActionPopVlan(),
-                                        parser.OFPActionOutput(port)])]
-                self.add_group(datapath, bkt, group_id,
+                bkt = [parser.OFPBucket(
+                       actions=[parser.OFPActionOutput(port)])]
+                self.add_group(datapath, bkt,
+                               self.l2_unfiltered_if_group(port),
                                ofproto.OFPGT_INDIRECT)
 
             # IN PORT 1, OUTPUT PORT 2
-            match = parser.OFPMatch(in_port=1, vlan_vid=n_vlan_match)
-            actions = [parser.OFPActionGroup(self.l2_if_group(2, n_vlan))]
+            match = parser.OFPMatch(in_port=1)
+            actions = [parser.OFPActionGroup(self.l2_unfiltered_if_group(2))]
             self.add_flow(datapath, 1000, match, actions)
 
             # IN PORT 2, OUTPUT PORT 1
-            match = parser.OFPMatch(in_port=2, vlan_vid=n_vlan_match)
-            actions = [parser.OFPActionGroup(self.l2_if_group(1, n_vlan))]
+            match = parser.OFPMatch(in_port=2)
+            actions = [parser.OFPActionGroup(self.l2_unfiltered_if_group(1))]
             self.add_flow(datapath, 1000, match, actions)
         except:
             traceback.print_exc()
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None,
-                 table_id=ACL_TABLE, goto=None):
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
-        if goto is not None:
-            inst.append(parser.OFPInstructionGotoTable(goto))
-
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
-                                    table_id=table_id,
+                                    table_id=self.ACL_TABLE,
                                     instructions=inst)
         else:
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst,
-                                    table_id=table_id)
+                                    table_id=self.ACL_TABLE)
         datapath.send_msg(mod)
 
     def add_group(self, datapath, buckets, group_id, group_type):
@@ -149,30 +111,18 @@ class SimplestSwitch13(app_manager.RyuApp):
 
         msg = parser.OFPGroupMod(datapath, ofproto.OFPGC_DELETE, 0,
                                  ofproto.OFPG_ALL)
-        api.send_msg(self, msg)
+        datapath.send_msg(msg)
 
     def del_flows(self, datapath):
         """ Deletes all existing flows """
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        msg = parser.OFPFlowMod(datapath=datapath,
-                                table_id=ofproto.OFPTT_ALL,
+        msg = parser.OFPFlowMod(datapath=datapath, table_id=ofproto.OFPTT_ALL,
                                 command=ofproto.OFPFC_DELETE,
                                 out_port=ofproto.OFPP_ANY,
                                 out_group=ofproto.OFPG_ANY)
-        api.send_msg(self, msg)
-
-    def l2_if_group(self, port, vlan_id):
-	""" An indirect OpenFlow interface with no VLAN filtering or tagging
-
-        This can be used to output packets out a specific port
-        id=[0:15] Port id --- [27:16] VLAN_ID --- [28:31] Type (0)
-        """
-        MAX_PORT = 0xFFFF
-        assert(port <= MAX_PORT)
-        port_vlan = (port | vlan_id << 16)
-        return self._ofdpa_group_id(port_vlan, 0)
+        datapath.send_msg(msg)
 
     def l2_unfiltered_if_group(self, port):
 	""" An indirect OpenFlow interface with no VLAN filtering or tagging
